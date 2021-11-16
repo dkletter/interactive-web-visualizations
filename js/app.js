21f32e7cfa5bc4dab61316dfb59a279f7444f96d
dkletter <dan@kletter.us>
2021-11-15 20:58:21 -0800

    Updating as I go

--- js/app.js
+++ js/app.js
@@ -1,6 +1,42 @@
 // Get the JSON endpoint
 const filepath = "data/samples.json";
 
-d3.json(filepath).then((data) => {
-    console.log(data);
-});
+function init(id) {
+
+    var selecter1 = d3.select("#selDataset")
+
+    d3.json(filepath).then((data)=> { 
+        var names = data.names;
+
+        names.forEach((id) => {
+            selecter1.append("option").text(id).property("value", id)
+        });
+
+        var blorp = names[0];
+            makePanel(blorp);        
+    });
+}
+
+function optionChanged(blorp) {
+    makePanel(blorp);
+}
+
+function makePanel(item) {
+
+    var selecter2 = d3.select("#sample-metadata");
+
+    d3.json(filepath).then((data) => {
+        var metadata = data.metadata;
+        var result1 = metadata.filter(items => items.id == item);
+        var demographics = result1[0];
+
+        selecter2.html("");
+
+        Object.entries(demographics).forEach(([key, value]) => {
+            selecter2.append("h5").text(`${key}: ${value}`);
+        });
+    });
+}
+
+init();
+
